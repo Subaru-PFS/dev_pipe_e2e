9611cd5c1fb8eb1bb524721b0d668c93b3a40776
Kiyoto Yabe <kiyoto.yabe@ipmu.jp>
2021-10-26 08:51:30 +0900

    add scripts for extended targets

---
+++ bin/run_pipe2d_integration_extended_test.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+#
+# Exercise the PFS 2D pipeline code.
+#
+# We run through the an example reduction to make sure everything's working.
+#
+# This script is taken from `pfs_pipe2d` and modified
+# 
+
+if [ $(uname -s) = Darwin ]; then
+    if [ -z $DYLD_LIBRARY_PATH ]; then
+        export DYLD_LIBRARY_PATH=$LSST_LIBRARY_PATH
+    fi
+fi
+
+usage() {
+    echo "Exercise the PFS 2D pipeline code" 1>&2
+    echo "" 1>&2
+    echo "Usage: $0 [-b <BRANCH>] [-r <RERUN>] [-d DIRNAME] [-c CORES] [-n] [-C] <PREFIX>" 1>&2
+    echo "" 1>&2
+    echo "    -b <BRANCH> : branch of drp_stella_data to use" 1>&2
+    echo "    -r <RERUN> : rerun name to use (default: 'integration')" 1>&2
+    echo "    -d <DIRNAME> : directory name to give data repo (default: 'INTEGRATION')" 1>&2
+    echo "    -c <CORES> : number of cores to use (default: 1)" 1>&2
+    echo "    -G : don't clone or update from git" 1>&2
+    echo "    -n : don't cleanup temporary products" 1>&2
+    echo "    -C : don't create calibs" 1>&2
+    echo "    <PREFIX> : directory under which to operate" 1>&2
+    echo "" 1>&2
+    exit 1
+}
+
+# Parse command-line arguments
+BRANCH=  # Branch to build
+RERUN="integration"  # Rerun name to use
+TARGET="INTEGRATION"  # Directory name to give data repo
+CORES=1  # Number of cores to use
+USE_GIT=true # checkout/update from git
+CLEANUP=true  # Clean temporary products?
+BUILD_CALIBS=true  # Build calibs?
+while getopts ":b:c:Cd:Gnr:" opt; do
+    case "${opt}" in
+        b)
+            BRANCH=${OPTARG}
+            ;;
+        c)
+            CORES=${OPTARG}
+            ;;
+        C)
+            BUILD_CALIBS=false
+            ;;
+        d)
+            TARGET=${OPTARG}
+            ;;
+        G)
+            USE_GIT=false
+            ;;
+        n)
+            CLEANUP=false
+            ;;
+        r)
+            RERUN=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+PREFIX=$1  # Directory to work in
+if [ -z "$PREFIX" ] || [ -n "$2" ]; then
+    usage
+fi
+
+set -evx
+
+mkdir -p $PREFIX
+cd $PREFIX
+TARGET="$(pwd)/$TARGET"
+
+if $USE_GIT; then
+    # Setting lfs.batch=true enables passwordless downloads with git-lfs.
+    if [ -e drp_stella_data ]; then
+        pushd drp_stella_data
+        git fetch --all --force --prune --tags
+        if [ -n $BRANCH ]; then
+            git -c lfs.batch=true checkout $BRANCH || echo "Can't checkout $BRANCH"
+        fi
+        popd
+    else
+        if [ -n $BRANCH ]; then
+            ( git -c lfs.batch=true clone --branch=$BRANCH --single-branch https://github.com/Subaru-PFS/drp_stella_data || git -c lfs.batch=true clone --branch=master --single-branch https://github.com/Subaru-PFS/drp_stella_data )
+        else
+            git -c lfs.batch=true clone --branch=master --single-branch https://github.com/Subaru-PFS/drp_stella_data
+        fi
+    fi
+
+    setup -jr drp_stella_data
+else
+    if [ -n $BRANCH ]; then
+    echo "Ignoring branch $BRANCH as you chose -G" >&2
+    fi
+fi
+
+if [ $CORES = 1 ]; then
+    batchArgs="--batch-type=none --doraise"
+    runArgs="--doraise"
+else
+    batchArgs="--batch-type=smp --cores $CORES --doraise"
+    runArgs="-j $CORES --doraise"
+fi
+
+if ( $CLEANUP ); then
+    cleanFlag="--clean"
+else
+    cleanFlag=""
+fi
+
+export OMP_NUM_THREADS=1
+drp_stella_data=${DRP_STELLA_DATA_DIR:-drp_stella_data}
+
+# Construct repo
+rm -rf $TARGET
+mkdir -p $TARGET
+mkdir -p $TARGET/CALIB
+[ -e $TARGET/_mapper ] || echo "lsst.obs.pfs.PfsMapper" > $TARGET/_mapper
+
+# Ingest images into repo
+ingestPfsImages.py $TARGET --mode=link \
+    $drp_stella_data/raw/PFFA*.fits \
+    -c clobber=True register.ignore=True
+
+# Ingest calibs
+sh ingest_calibs.sh
+
+# End-to-end pipeline
+# science frames for brn
+generateCommands.py "$TARGET" \
+    "$PFS_PIPE2D_DIR"/examples/integration_extended_test.yaml \
+    science_for_brn.sh \
+    --rerun="$RERUN" --blocks=science_for_brn \
+    -j "$CORES" $cleanFlag
+sh science_for_brn.sh
+
+# science frames for bmn
+generateCommands.py "$TARGET" \
+    "$PFS_PIPE2D_DIR"/examples/integration_extended_test.yaml \
+    science_for_bmn.sh \
+    --rerun="$RERUN" --blocks=science_for_bmn \
+    -j "$CORES" $cleanFlag
+sh science_for_bmn.sh
+
+echo "Done."
---
+++ bin/test_pipe_e2e_extended.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+usage() {
+    # Display usage and quit
+    echo "Test the PFS pipeline end-to-end process." 1>&2
+    echo "" 1>&2
+    echo "Usage: $0 [-r <RERUN>] [-d DIRNAME] [-t <TAG1D>] [-c CORES] <PREFIX>" 1>&2
+    echo "" 1>&2
+    echo "    -r <RERUN> : rerun name to use (default: 'integration')" 1>&2
+    echo "    -d <DIRNAME> : directory name to give data repo (default: 'INTEGRATION')" 1>&2
+    echo "    -t <TAG1D> : directory name to put 1D results (default: pipe1d_workdir)" 1>&2
+    echo "    -c <CORES> : number of cores to use (default: 1)" 1>&2
+    echo "    -G : don't clone or update from git" 1>&2
+    echo "    -k : skip 2D procedure" 1>&2
+    echo "    <PREFIX> : directory under which to operate" 1>&2
+    echo "" 1>&2
+    exit 1
+}
+
+# Parse command-line arguments
+SERVER="gfarm"
+RERUN="integration"  # Rerun name to use
+TARGET="INTEGRATION"  # Directory name to give data repo
+TAG1D="pipe1d_workdir" # Directory name for 1D results
+SKIP2D=false
+CORES=1  # Number of cores to use
+USE_GIT=true # checkout/update from git
+while getopts ":c:t:d:Gkr:" opt; do
+    case "${opt}" in
+        c)
+            CORES=${OPTARG}
+            ;;
+        d)
+            TARGET=${OPTARG}
+            ;;
+        G)
+            USE_GIT=false
+            ;;
+        r)
+            RERUN=${OPTARG}
+            ;;
+        t)
+            TAG1D=${OPTARG}
+            ;;
+        k)
+            SKIP2D=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+PREFIX=$1
+if [ -z "$PREFIX" ] || [ -n "$2" ]; then
+    usage
+fi
+BASEDIR="$(pwd)"
+
+if [ "$SERVER" = "gfarm" ]; then
+    # run 2D pipeline
+    if [ "$SKIP2D" = false ]; then
+        if $USE_GIT; then
+            #pfs_integration_test.sh -r $RERUN -d $TARGET -c $CORES $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+            run_pipe2d_integration_extended_test.sh -r $RERUN -d $TARGET -c $CORES $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+        else
+            cd $BASEDIR
+            setup -jr drp_stella_data
+            #pfs_integration_test.sh -r $RERUN -d $TARGET -c $CORES -G $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+            run_pipe2d_integration_extended_test.sh -r $RERUN -d $TARGET -c $CORES -G $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+        fi
+    fi
+    # find directories where pfsOBject files exist
+    cd $BASEDIR
+    mkdir -p $TAG1D
+    mkdir -p $TAG1D/output
+    cp -r calibration $TAG1D
+    PFSOBJECT_DIR="$(pwd)"/$TARGET/rerun/$RERUN/pipeline/pfsObject/
+    cd $PFSOBJECT_DIR
+    find ./ -name "pfsObject*.fits" | awk '{gsub("/pfsObject"," ");print $1}' | uniq | while read dir; do   
+        # run 1D pipeline
+        echo $dir
+        cd $PFSOBJECT_DIR
+        drp_1dpipe --workdir=$BASEDIR/$TAG1D --spectra_dir=$PFSOBJECT_DIR/$dir --output_dir=$BASEDIR/$TAG1D/output --parameters_file=$BASEDIR/parameters_e2e.json --concurrency=$CORES --loglevel=INFO 2>&1 | tee $BASEDIR/test_e2e_pipe1d.log
+    done
+    cd $BASEDIR
+elif [ "$SERVER" = "docker" ]; then
+    # run 2D pipeline
+    if [ "$SKIP2D" = false ]; then
+        if $USE_GIT; then
+            #pfs_integration_test.sh -r $RERUN -d $TARGET -c $CORES $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+            run_pipe2d_integration_test.sh -r $RERUN -d $TARGET -c $CORES $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+        else
+            cd $BASEDIR
+            setup -jr drp_stella_data
+            #pfs_integration_test.sh -r $RERUN -d $TARGET -c $CORES -G $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+            run_pipe2d_integration_test.sh -r $RERUN -d $TARGET -c $CORES -G $PREFIX 2>&1 | tee test_e2e_pipe2d.log
+        fi
+    fi
+    # find directories where pfsOBject files exist
+    cd $BASEDIR
+    mkdir -p $TAG1D
+    mkdir -p $TAG1D/output
+    cp -r calibration $TAG1D
+    PFSOBJECT_DIR="$(pwd)"/$TARGET/rerun/$RERUN/pipeline/pfsObject/
+    cd $PFSOBJECT_DIR
+    find ./ -name "pfsObject*.fits" | awk '{gsub("/pfsObject"," ");print $1}' | uniq | while read dir; do   
+        # run 1D pipeline
+        echo $dir
+        cd $PFSOBJECT_DIR
+        drp_1dpipe --workdir=$BASEDIR/$TAG1D --spectra_dir=$PFSOBJECT_DIR/$dir --output_dir=$BASEDIR/$TAG1D/output --parameters_file=$BASEDIR/parameters_e2e.json --concurrency=$CORES --loglevel=INFO 2>&1 | tee $BASEDIR/test_e2e_pipe1d.log
+    done
+    cd $BASEDIR
+else
+    echo "Use gfarm or docker server"
+fi
+
+echo ""
+echo "All done."
+echo ""
+
